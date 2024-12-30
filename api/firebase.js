bf291de6555cb2acebc59d291c20855ae6129476
18yyy <166515457+18yyy@users.noreply.github.com>
2024-12-30 04:23:33 -0300

    Update firebase.js

--- api/firebase.js
+++ api/firebase.js
@@ -1,21 +1,28 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import admin from 'firebase-admin';
 
-// Suas credenciais
-const firebaseConfig = {
-  apiKey: "AIzaSyB-8m3tHeUnkEPg19WYwepNQp7M80LycM4",
-  authDomain: "hamburgueria-69d7a.firebaseapp.com",
-  projectId: "hamburgueria-69d7a",
-  storageBucket: "hamburgueria-69d7a.firebasestorage.app",
-  messagingSenderId: "501082499235",
-  appId: "1:501082499235:web:b779bdef0d329edd05f716",
-  measurementId: "G-VX5Y8D8SZL"
+// Inicializando o Firebase Admin SDK
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
 };
 
-// Inicializa o Firebase
-const app = initializeApp(firebaseConfig);
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+  });
+} else {
+  admin.app(); // Usar o app Firebase existente
+}
 
-// Inicializa o Firestore
-const db = getFirestore(app);
+const db = admin.firestore();
 
 export { db };
